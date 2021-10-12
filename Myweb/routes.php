e563dd08d205dadbc4e580f39c01e593898e6eb0
Potjarinn <potjarin.p@ku.th>
2021-10-13 01:51:29 +0700

    Create routes.php

---
+++ Myweb/routes.php
@@ -0,0 +1,25 @@
+<?php
+$controllers = array('home'=>['home','error'],'hospital'=>['index','newhospital','addhospital',
+'updateForms','update','deleteconfirm','delete']);
+
+function call($controller,$action){
+    require_once("controllers/".$controller."_controller.php");
+    switch($controller){
+        case "home": $controller = new HomeController();break;
+        case "hospital": require_once("./models/hospitalmodels.php");
+                         //require_once("./models/hospitalmodels.php");
+            $controller = new hospital();break;
+    }
+    $controller->{$action}();
+}
+
+if(array_key_exists($controller,$controllers)){
+    if(in_array($action,$controllers[$controller])){
+        call($controller,$action);
+    }else{
+        call('home','error');
+    }
+}else{
+    call('home','error');
+}
+?>
