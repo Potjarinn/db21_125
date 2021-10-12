106855fa23cc2e575d33af6b1c4757ee555cec92
Potjarinn <potjarin.p@ku.th>
2021-10-13 01:50:18 +0700

    Create index.php

---
+++ Myweb/index.php
@@ -0,0 +1,21 @@
+<?php
+if(isset($_GET['controller'])&&isset($_GET['action']))
+{
+    $controller = $_GET['controller'];
+    $action = $_GET['action'];
+}else
+{
+    $controller = 'home';
+    $action = 'home';
+}
+?>
+</head>
+<body>
+<?php echo "controller = ".$controller.",action = ".$action; ?>
+<br><a href="?controller=home&action=home">Home</a>
+    <a href="?controller=hospital&action=index">Hospital</a>
+<?php require_once("routes.php"); ?>
+
+
+</body>
+</html>
