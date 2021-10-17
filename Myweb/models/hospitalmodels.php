0f1dfe58d6d19d3b083d7872b6a941d39b02fb23
Potjarinn <potjarin.p@ku.th>
2021-10-17 23:30:54 +0700

    Update hospitalmodels.php

--- Myweb/models/hospitalmodels.php
+++ Myweb/models/hospitalmodels.php
@@ -1,20 +1,20 @@
 <?php
 class hospitalModels{
     public $hospital_id;
-    public $name;
-    public $phone;
-    public $address;
-    public $county;
-    public $province;
+    public $name_H;
+    public $phone_H;
+    public $address_H;
+    public $county_H;
+    public $province_H;
 
-    public function __construct($hospital_id,$name,$phone,$address,$county,$province)
+    public function __construct($hospital_id,$name_H,$phone_H,$address_H,$county_H,$province_H)
     {
         $this->hospital_id = $hospital_id;
-        $this->name = $name;
-        $this->phone = $phone;
-        $this->address = $address;
-        $this->county = $county;
-        $this->province = $province;
+        $this->name_H = $name_H;
+        $this->phone_H = $phone_H;
+        $this->address_H = $address_H;
+        $this->county_H = $county_H;
+        $this->province_H = $province_H;
     }
 
     public static function get($hospital_id)
@@ -24,14 +24,14 @@ class hospitalModels{
         $result = $conn->query($sql);
         $my_row = $result->fetch_assoc();
         $hospital_id = $my_row[hospital_id];
-        $name = $my_row[name];
-        $phone = $my_row[phone];
-        $address = $my_row[address];
-        $county = $my_row[county];
-        $province = $my_row[province];
+        $name_H = $my_row[name_H];
+        $phone_H = $my_row[phone_H];
+        $address_H = $my_row[address_H];
+        $county_H = $my_row[county_H];
+        $province_H = $my_row[province_H];
         require("connection_close.php");
 
-        return new hospitalModels($hospital_id,$name,$phone,$address,$county,$province);
+        return new hospitalModels($hospital_id,$name_H,$phone_H,$address_H,$county_H,$province_H);
     }
 
     public static function getAll()
@@ -43,34 +43,58 @@ class hospitalModels{
         while($my_row = $result->fetch_assoc())
         {
             $hospital_id = $my_row[hospital_id];
-            $name = $my_row[name];
-            $phone = $my_row[phone];
-            $address = $my_row[address];
-            $county = $my_row[county];
-            $province = $my_row[province];
-            $hospitalmodelList[] = new hospitalModels($hospital_id,$name,$phone,$address,$county,$province);
+            $name_H = $my_row[name_H];
+            $phone_H = $my_row[phone_H];
+            $address_H = $my_row[address_H];
+            $county_H = $my_row[county_H];
+            $province_H = $my_row[province_H];
+            $hospitalmodelList[] = new hospitalModels($hospital_id,$name_H,$phone_H,$address_H,$county_H,$province_H);
         }
         require("connection_close.php");
 
         return $hospitalmodelList;
     }
 
+    public static function search($key)
+    {
+        $hospitalmodelList  = [];
+        echo "gg";
+        require("connection_connect.php");
+        $sql = "SELECT * FROM `Hospital` where (hospital_id like %'$key'% or name_H like %'$key'% or phone_H like %'$key'% OR address_H like %'$key'% or county_H like %'$key'% or province_H like %'$key'%)";
+        $result = $conn->query($sql);
+        echo "aa";
+        while($my_row = $result->fetch_assoc())
+        {
+            $hospital_id = $my_row[hospital_id];
+            $name_H = $my_row[name_H];
+            $phone_H = $my_row[phone_H];
+            $address_H = $my_row[address_H];
+            $county_H = $my_row[county_H];
+            $province_H = $my_row[province_H];
+            $hospitalmodelList[] = new hospitalModels($hospital_id,$name_H,$phone_H,$address_H,$county_H,$province_H);
+        }
+        require("connection_close.php");
+        echo "uu";
+        return $hospitalmodelList;
+       
+    }
 
-    public static function Add($hospital_id,$name,$phone,$address,$county,$province)
+
+    public static function Add($hospital_id,$name_H,$phone_H,$address_H,$county_H,$province_H)
     {
         require("connection_connect.php");
-        $sql = "INSERT INTO `Hospital`(`hospital_id`, `name`, `phone`, `address`, `county`, `province`) 
-        VALUES ('$hospital_id','$name','$phone',$address,'$county','$province')";
+        $sql = "INSERT INTO `Hospital`(`hospital_id`, `name_H`, `phone_H`, `address_H`, `county_H`, `province_H`) 
+        VALUES ('$hospital_id','$name_H','$phone_H','$address_H','$county_H','$province_H')";
         $result = $conn->query($sql);
         require("connection_close.php");
         return ;
     }
 
-    public static function update($hospital_id,$name,$phone,$address,$county,$province,$hospitalid)
+    public static function update($hospital_id,$name_H,$phone_H,$address_H,$county_H,$province_H,$hospitalid)
     {
         require("connection_connect.php");
-        $sql = "UPDATE `Hospital` SET `hospital_id`='$hospital_id',`name`='$name',`phone`='$phone',
-        `address`=$address,`county`='$county',`province`='$province' WHERE hospital_id='$hospitalid' ";
+        $sql = "UPDATE `Hospital` SET `hospital_id`='$hospital_id',`name_H`='$name_H',`phone_H`='$phone_H',
+        `address_H`=$address_H,`county_H`='$county_H',`province_H`='$province_H' WHERE hospital_id='$hospitalid' ";
         $result = $conn->query($sql);
         require("connection_close.php");
         return "update success $result row";
