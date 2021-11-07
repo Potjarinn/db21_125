6bde267bcfb3d91e19198d935bc64b336d0efe7a
Potjarinn <potjarin.p@ku.th>
2021-11-07 18:10:08 +0700

    Update index.php

--- Myweb/index.php
+++ Myweb/index.php
@@ -14,6 +14,7 @@ if(isset($_GET['controller'])&&isset($_GET['action']))
 <?php echo "controller = ".$controller.",action = ".$action; ?>
 <br><a href="?controller=home&action=home">[Home]</a>
     <a href="?controller=hospital&action=index">[Hospital]</a>
+    <a href="?controller=doctor&action=index">[Doctor]</a>
 <?php require_once("routes.php"); ?>
 
 
