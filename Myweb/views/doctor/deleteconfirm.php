5f0baa17cee140fd4e935193cbc1e886cc88517c
Potjarinn <potjarin.p@ku.th>
2021-11-08 03:59:52 +0700

    Create deleteconfirm.php

---
+++ Myweb/views/doctor/deleteconfirm.php
@@ -0,0 +1,11 @@
+<?php echo "<br>Are you sure to delete this student <br>
+            <br>  doctor_id:$doctormodels->doctor_id  
+            <br>name:$doctormodels->name_D  
+            <br>phone:$doctormodels->phone_D  
+            <br>hospital:$doctormodels->name_H<br>";?>
+<form method="get" action="">
+    <input type="hidden" name="controller" value="doctor"/>
+    <input type="hidden" name="doctorid" value="<?php echo $doctormodels->doctor_id;?>"/>
+    <button type="submit" name="action" value="index">Back</button>
+    <button type="submit" name="action" value="delete">Delete</button>
+</form>
