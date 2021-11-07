5dd49c709dba33bd0bcdf0bbf1a1856f8336e6b4
Potjarinn <potjarin.p@ku.th>
2021-11-08 04:01:04 +0700

    Create updateform.php

---
+++ Myweb/views/doctor/updateform.php
@@ -0,0 +1,25 @@
+<form method="get" action="">
+
+    <label>doctor_id <input type="text" name="doctor_id"
+        value="<?php echo  $doctormodels->doctor_id;?>"/> </label><br>
+
+    <label>name <input type="text" name="name_D"
+        value="<?php echo  $doctormodels->name_D;?>"/> </label><br>
+
+    <label>phone <input type="text" name="phone_D"
+        value="<?php echo  $doctormodels->phone_D;?>"/> </label><br>
+
+    <label>hospital <select name="hospital_id">
+        <?php foreach($hospitalmodellist as $hospital){
+            echo "<option value=$hospital->hospital_id";
+            if($hospital->hospital_id==$doctormodels->hospital_id){
+                echo "selected='selected'";
+            }
+            echo ">$hospital->name_H</option>";
+        }?>
+        </select></label><br>    
+    <input type="hidden" name="controller" value="doctor"/>
+    <input type="hidden" name="doctorid" value="<?php echo $doctormodels->doctor_id;?>"/>
+    <button type="submit" name="action" value="index"> Back </button>
+    <button type="submit" name="action" value="update"> Update </button>
+</form>
