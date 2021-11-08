2b81765b42c62830a1dfd357979d2358ebdad29b
Potjarinn <potjarin.p@ku.th>
2021-11-08 22:25:09 +0700

    Update updateform.php

--- Myweb/views/homeisolation/updateform.php
+++ Myweb/views/homeisolation/updateform.php
@@ -1,9 +1,13 @@
 <form method="get" action="">
+        
+<?php foreach($patienmodellist as $patien){
+            echo $patien->patient_id;
+        }?>
 
-    <label>IDcard <input type="text" name="patien_id">
+    <label>IDcard <input type="text" name="patient_id">
         <?php foreach($patienmodellist as $patien){
-            echo "<option value=$patien->patien_id";
-            if($patien->patien_id==$homeisolationmodels->patien_id){
+            echo "<option value=$patien->patient_id";
+            if($patien->patient_id==$homeisolationmodels->patient_id){
                 echo "selected='selected'";
             }
             echo ">$patien->id_card</option>";
@@ -13,16 +17,22 @@
     <label>Address <input type="text" name="address_Home"
         value="<?php echo  $homeisolationmodels->address_Home;?>"/> </label><br>
 
+    <label>County <input type="text" name="county_Home"
+        value="<?php echo  $homeisolationmodels->county_Home;?>"/> </label><br>
+
+    <label>Province <input type="text" name="province_Home"
+        value="<?php echo  $homeisolationmodels->province_Home;?>"/> </label><br>
+
     <label>Color <input type="text" name="color_name"
-        value="<?php echo  $homeisolationmodels->phone_D;?>"/> </label><br>
+        value="<?php echo  $homeisolationmodels->color_name;?>"/> </label><br>
     
     <label>IDatk <select name="id_atk">
-        <?php foreach($patienmodellist as $atk){
+        <?php foreach($atkmodellist as $atk){
             echo "<option value=$atk->id_atk";
             if($atk->id_atk==$homeisolationmodels->id_atk){
                 echo "selected='selected'";
             }
-           // echo ">$atk->name_H</option>";
+            echo ">$atk->id_atk</option>";
         }?>
         </select></label><br> 
         
@@ -46,7 +56,7 @@
         }?>
         </select></label><br>    
     <input type="hidden" name="controller" value="homeisolation"/>
-    <input type="hidden" name="patienid" value="<?php echo $homeisolationmodels->patien_id;?>"/>
+    <input type="hidden" name="patientid" value="<?php echo $homeisolationmodels->patient_id;?>"/>
     <button type="submit" name="action" value="index"> Back </button>
     <button type="submit" name="action" value="update"> Update </button>
 </form>
