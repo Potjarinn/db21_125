241f6926178910a543b79913d83d71f338c99ff1
Potjarinn <potjarin.p@ku.th>
2021-11-10 03:06:39 +0700

    Update newdoctor.php

--- Myweb/views/doctor/newdoctor.php
+++ Myweb/views/doctor/newdoctor.php
@@ -3,7 +3,7 @@
     <label>รหัสแพทย์อาสา<input type="text" name="doctor_id"/> </label><br>
     <label>ชื่อแพทย์อาสา<input type="text" name="name_D"/> </label><br>
     <label>หมายเลขโทรศัพท์<input type="text" name="phone_D"/> </label><br>
-    <label>โรงพยาบาล<select name="hospital_id">
+    <label>สังกัดโรงพยาบาล<select name="hospital_id">
         <option value="">choose</option>
         <?php foreach($hospitalmodellist2 as $newhospital){
             echo "<option value=$newhospital->hospital_id>$newhospital->name_H</option>";
