d086ffd8a3a933cdbc9b96b52f188d74db6294a6
Potjarinn <potjarin.p@ku.th>
2021-11-10 00:05:22 +0700

    Update newdoctor.php

--- Myweb/views/doctor/newdoctor.php
+++ Myweb/views/doctor/newdoctor.php
@@ -1,9 +1,9 @@
 <form method="get" action="">
     <br>
-    <label>Doctor_id<input type="text" name="doctor_id"/> </label><br>
-    <label>Name<input type="text" name="name_D"/> </label><br>
-    <label>Phone<input type="text" name="phone_D"/> </label><br>
-    <label>Hospital<select name="hospital_id">
+    <label>รหัสแพทย์อาสา<input type="text" name="doctor_id"/> </label><br>
+    <label>ชื่อแพทย์อาสา<input type="text" name="name_D"/> </label><br>
+    <label>หมายเลขโทรศัพท์<input type="text" name="phone_D"/> </label><br>
+    <label>โรงพยาบาล<select name="hospital_id">
         <option value="">choose</option>
         <?php foreach($hospitalmodellist2 as $newhospital){
             echo "<option value=$newhospital->hospital_id>$newhospital->name_H</option>";
