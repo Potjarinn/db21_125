e47d326b51321041514e8c16e814221f1fe1d0af
Potjarinn <potjarin.p@ku.th>
2021-11-10 00:04:34 +0700

    Update deleteconfirm.php

--- Myweb/views/doctor/deleteconfirm.php
+++ Myweb/views/doctor/deleteconfirm.php
@@ -1,8 +1,8 @@
 <?php echo "<br>Are you sure to delete this Doctor? <br>
-            <br>  Doctor_id:$doctormodels->doctor_id  
-            <br>Name:$doctormodels->name_D  
-            <br>Phone:$doctormodels->phone_D  
-            <br>Hospital:$doctormodels->name_H<br>";?>
+            <br>  รหัสแพทย์อาสา:$doctormodels->doctor_id  
+            <br>ชื่อแพทย์อาสา:$doctormodels->name_D  
+            <br>หมายเลขโทรศัพท์:$doctormodels->phone_D  
+            <br>โรงพยาบาล:$doctormodels->name_H<br>";?>
 <form method="get" action="">
     <input type="hidden" name="controller" value="doctor"/>
     <input type="hidden" name="doctorid" value="<?php echo $doctormodels->doctor_id;?>"/>
