47e6133a2d16f1e36702b21f7dfe8684131733b7
Potjarinn <potjarin.p@ku.th>
2021-11-10 00:25:50 +0700

    Update deleteconfirm.php

--- Myweb/views/hospital/deleteconfirm.php
+++ Myweb/views/hospital/deleteconfirm.php
@@ -1,10 +1,10 @@
 <?php echo "<br>Are you sure to delete this Hospital? <br>
-            <br>  Hospital_id:$hospitalmodels->hospital_id  
-            <br>Name:$hospitalmodels->name_H  
-            <br>Phone:$hospitalmodels->phone_H 
-            <br>Address:$hospitalmodels->address_H  
-            <br>County:$hospitalmodels->county_H 
-            <br>Province:$hospitalmodels->province_H<br>";?>
+            <br>  รหัสโรงพยาบาล:$hospitalmodels->hospital_id  
+            <br>ชื่อโรงพยาบาล:$hospitalmodels->name_H  
+            <br>หมายเลขโทรศัพท์:$hospitalmodels->phone_H 
+            <br>ที่อยู่:$hospitalmodels->address_H  
+            <br>เขต:$hospitalmodels->county_H 
+            <br>จังหวัด:$hospitalmodels->province_H<br>";?>
 <form method="get" action="">
     <input type="hidden" name="controller" value="hospital"/>
     <input type="hidden" name="hospitalid" value="<?php echo $hospitalmodels->hospital_id;?>"/>
