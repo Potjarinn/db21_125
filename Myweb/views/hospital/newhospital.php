62688bebc65d0ae64a49c96c483a51e001676faa
Potjarinn <potjarin.p@ku.th>
2021-11-10 00:32:50 +0700

    Update newhospital.php

--- Myweb/views/hospital/newhospital.php
+++ Myweb/views/hospital/newhospital.php
@@ -1,11 +1,11 @@
 <form method="get" action="">
     <br>
-    <label>Hospital_id<input type="text" name="hospital_id"/> </label><br>
-    <label>Name<input type="text" name="name_H"/> </label><br>
-    <label>Phone<input type="text" name="phone_H"/> </label><br>
-    <label>Address<input type="text" name="address_H"/> </label><br>
-    <label>County<input type="text" name="county_H"/> </label><br>
-    <label>Province<input type="text" name="province_H"/> </label><br>
+    <label>รหัสโรงพยาบาล<input type="text" name="hospital_id"/> </label><br>
+    <label>ชื่อโรงพยาบาล<input type="text" name="name_H"/> </label><br>
+    <label>หมายเลขโทรศัพท์<input type="text" name="phone_H"/> </label><br>
+    <label>ที่อยู่<input type="text" name="address_H"/> </label><br>
+    <label>เขต<input type="text" name="county_H"/> </label><br>
+    <label>จังหวัด<input type="text" name="province_H"/> </label><br>
     
     <input type="hidden" name="controller" value="hospital"/>
     <button type="submit" name="action" value="index">Back</button>
