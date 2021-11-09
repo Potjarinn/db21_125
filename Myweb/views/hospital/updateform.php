51a9430f3309c2a6c3177beb9aed4bb504ad3172
Potjarinn <potjarin.p@ku.th>
2021-11-09 15:42:35 +0700

    Update updateform.php

--- Myweb/views/hospital/updateform.php
+++ Myweb/views/hospital/updateform.php
@@ -1,21 +1,21 @@
 <form method="get" action="">
 
-    <label>name <input type="text" name="name_H"
+    <label>Name <input type="text" name="name_H"
         value="<?php echo  $hospitalmodels->name_H;?>"/> </label><br>
 
-    <label>phone <input type="text" name="phone_H"
+    <label>Phone <input type="text" name="phone_H"
         value="<?php echo  $hospitalmodels->phone_H;?>"/> </label><br>
 
-    <label>address <input type="text" name="address_H"
+    <label>Address <input type="text" name="address_H"
         value="<?php echo  $hospitalmodels->address_H;?>"/> </label><br>
         
-    <label>county <input type="text" name="county_H"
+    <label>County <input type="text" name="county_H"
         value="<?php echo  $hospitalmodels->county_H;?>"/> </label><br>
 
-    <label>province <input type="text" name="province_H"
+    <label>Province <input type="text" name="province_H"
         value="<?php echo  $hospitalmodels->province_H;?>"/> </label><br>
 
-    <label>hospital_id <input type="text" name="hospital_id"
+    <label>Hospital_id <input type="text" name="hospital_id"
         value="<?php echo  $hospitalmodels->hospital_id;?>"/> </label><br>
        
     <input type="hidden" name="controller" value="hospital"/>
