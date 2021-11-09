6dab7cd527e2b3673f92faea37a76473f4e55fec
Potjarinn <potjarin.p@ku.th>
2021-11-09 15:35:56 +0700

    Update home.php

--- Myweb/views/home/home.php
+++ Myweb/views/home/home.php
@@ -1,1 +1,20 @@
-<p>Welcome</p>
+
+<br>
+Welcome HOME
+</body></form></html>
+<table border = 3>
+<tr> <td>IDcard</td>
+<td>Name</td>
+<td>Lastname</td>
+<td>Doctor</td>
+<td>Hospital</td>
+<?php foreach( $homeisolationmodellist as  $homelist)
+{
+    echo "<tr> <td>$homelist->id_card</td>
+    <td>$homelist->NamePeople</td>
+    <td>$homelist->LastnameP</td>
+    <td>$homelist->name_D</td> 
+    <td>$homelist->name_H</td> </tr>";
+}
+echo "</table>";
+ ?>
