1766e087ff4a316c2941e52235692a3a9551c3a1
Potjarinn <potjarin.p@ku.th>
2021-11-09 15:39:18 +0700

    Update newhomeisolation.php

--- Myweb/views/homeisolation/newhomeisolation.php
+++ Myweb/views/homeisolation/newhomeisolation.php
@@ -1,36 +1,47 @@
 <form method="get" action="">
     <br>
     <label>IDcard<select name="patient_id">
+        <option value="">choose</option>
         <?php foreach($patienmodellist4 as $newpatien){
             echo "<option value=$newpatien->patient_id>$newpatien->id_card</option>";
         }?>
         </select></label><br>
 
-    <label>Name<input type="text" name="NamePeople"/> </label><br>
-    <label>Lastname<input type="text" name="LastnameP"/> </label><br>
+
+    <label>Name<select name="id_card">
+        <option value="">choose</option>
+        <?php foreach($peoplemodellist7 as $namep){
+            echo "<option value=$namep->id_card>$namep->NamePeople $namep->LastnameP</option>";
+        }?>
+        </select></label><br>
+
     <label>Address<input type="text" name="address_Home"/> </label><br>
     <label>County<input type="text" name="county_Home"/> </label><br>
     <label>Province<input type="text" name="province_Home"/> </label><br>
 
     <label>Color<select name="color_name">
+        <option value="">choose</option>
         <?php foreach($colormodellist6 as $newcolor){
             echo "<option value=$newcolor->color_name>$newcolor->color_name</option>";
         }?>
         </select></label><br>
     
     <label>IDatk<select name="id_atk">
+        <option value="">choose</option>
         <?php foreach($atkmodellist5 as $newatk){
             echo "<option value=$newatk->id_atk>$newatk->id_atk</option>";
         }?>
         </select></label><br>
 
     <label>Doctor<select name="doctor_id">
+        <option value="">choose</option>
         <?php foreach($doctormodellist3 as $newdoctor){
             echo "<option value=$newdoctor->doctor_id>$newdoctor->name_D</option>";
         }?>
         </select></label><br>
 
     <label>Hospital<select name="hospital_id">
+        <option value="">choose</option>
         <?php foreach($hospitalmodellist2 as $newhospital){
             echo "<option value=$newhospital->hospital_id>$newhospital->name_H</option>";
         }?>
