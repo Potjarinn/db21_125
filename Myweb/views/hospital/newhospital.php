dadf58195afb8177badc82723fadbd4f0fdf0e2b
Potjarinn <potjarin.p@ku.th>
2021-11-10 03:12:38 +0700

    Update newhospital.php

--- Myweb/views/hospital/newhospital.php
+++ Myweb/views/hospital/newhospital.php
@@ -3,7 +3,7 @@
     <label>รหัสโรงพยาบาล<input type="text" name="hospital_id"/> </label><br>
     <label>ชื่อโรงพยาบาล<input type="text" name="name_H"/> </label><br>
     <label>หมายเลขโทรศัพท์<input type="text" name="phone_H"/> </label><br>
-    <label>ที่อยู่<input type="text" name="address_H"/> </label><br>
+    <label>ที่อยู่โรงพยาบาล<input type="text" name="address_H"/> </label><br>
     <label>เขต<input type="text" name="county_H"/> </label><br>
     <label>จังหวัด<input type="text" name="province_H"/> </label><br>
     
