d9e5dd13ca816bc1ce32906aad43bc0db6eb03a9
Potjarinn <potjarin.p@ku.th>
2021-11-10 15:13:22 +0700

    Create deleteconfirm.php

---
+++ views/hospital/deleteconfirm.php
@@ -0,0 +1,13 @@
+<?php echo "<br>Are you sure to delete this Hospital? <br>
+            <br>  รหัสโรงพยาบาล:$hospitalmodels->hospital_id  
+            <br>ชื่อโรงพยาบาล:$hospitalmodels->name_H  
+            <br>หมายเลขโทรศัพท์:$hospitalmodels->phone_H 
+            <br>ที่อยู่โรงพยาบาล:$hospitalmodels->address_H  
+            <br>เขต:$hospitalmodels->county_H 
+            <br>จังหวัด:$hospitalmodels->province_H<br>";?>
+<form method="get" action="">
+    <input type="hidden" name="controller" value="hospital"/>
+    <input type="hidden" name="hospitalid" value="<?php echo $hospitalmodels->hospital_id;?>"/>
+    <button type="submit" name="action" value="index">Back</button>
+    <button type="submit" name="action" value="delete">Delete</button>
+</form>
