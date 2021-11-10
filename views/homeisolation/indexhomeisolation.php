c647d0b6639c9b00a8e0df734a924a76429a2422
Potjarinn <potjarin.p@ku.th>
2021-11-10 15:12:12 +0700

    Create indexhomeisolation.php

---
+++ views/homeisolation/indexhomeisolation.php
@@ -0,0 +1,35 @@
+
+<br><br>new Homeisolation <a href=?controller=homeisolation&action=newhomeisolation>Click</a><br>
+<form method="get" action="">
+    <input type="text" name="key">
+    <input type="hidden" name="controller" value="homeisolation"/>
+    <button type="submit" name="action" value="search">
+Search</button>
+</body></form></html>
+<table border = 1>
+<tr> <td><b>เลขบัตรประชาชน</td>
+<td><b>ชื่อผู้ป่วย</td>
+<td><b>ที่อยู่ผู้ป่วย</td>
+<td><b>เขต</td>
+<td><b>จังหวัด</td>
+<td><b>สีของอาการ</td>
+<td><b>รหัสATK</td>
+<td><b>ชื่อแพทย์อาสาที่ดูแล</td>
+<td><b>ชื่อโรงพยาบาลที่ดูแล</td>
+<td><b>Update</td><td><b>Delete</td> </tr>
+<?php foreach( $homeisolationmodellist as  $homelist)
+{
+    echo "<tr> <td>$homelist->id_card</td>
+    <td>$homelist->NamePeople $homelist->LastnameP</td>
+    <td>$homelist->address_Home</td>
+    <td>$homelist->county_Home</td> 
+    <td>$homelist->province_Home</td>  
+    <td>$homelist->color_name</td> 
+    <td>$homelist->id_atk</td> 
+    <td>$homelist->name_D</td> 
+    <td>$homelist->name_H</td> 
+    <td>  <a href=?controller=homeisolation&action=updateforms&patient_id=$homelist->patient_id> update </a> </td>
+    <td>  <a href=?controller=homeisolation&action=deleteconfirm&patient_id=$homelist->patient_id> delete</a> </td></tr>";
+}
+echo "</table>";
+ ?>
